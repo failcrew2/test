0d85a2f8c66124f619aab48f9450109fc1f4bbee
Tima <failcrew2@icloud.com>
2022-07-06 23:43:38 +0300

    Сделали приветствие

---
+++ Examples_005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользавателя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура это же маша");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
