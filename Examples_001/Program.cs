472b210da153395f9e0aa2d2c0519194f39f09b0
Tima <failcrew2@icloud.com>
2022-07-14 00:41:27 +0300

    Дз и 3 задачи

--- Examples_001/Program.cs
+++ Examples_001/Program.cs
@@ -1,29 +1,43 @@
-﻿
- Console.WriteLine("Введите первое число");
- int numA = Convert.ToInt32(Console.ReadLine());
- Console.WriteLine("Введите второе число"); 
- int numB = Convert.ToInt32(Console.ReadLine());
-   if (numA > numB)
-   Console.WriteLine(numA);
-   else 
-   Console.WriteLine(numB);
-
- 
-
-
-  
+﻿// Задача 1 
+//  Console.WriteLine("Введите первое число");
+//    int numA = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Введите второе число"); 
+//    int numB = Convert.ToInt32(Console.ReadLine());
+//       if (numA > numB)
+//    Console.WriteLine(numA);
+//       else 
+//    Console.WriteLine(numB);
 
 
 
 
 
+// Задача 2
+// Console.WriteLine("Введите первое число");
+//   int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+//   int numB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+//   int numC = Convert.ToInt32(Console.ReadLine());
+//   int max = 0;
+
+//     if (numA >= numB && numA >= numC)
+//  max = numA;
+//     else if (numB >= numA && numB >=numC )
+//  max = numB;
+//     else if (numC >= numA && numC >= numB)
+//  max = numC;
+// Console.WriteLine (max );
 
 
 
 
 
 
-  
+// Задача 3
+// Console.WriteLine("Введите число : ");
+// int numA = Convert.ToInt32(Console.Read());
+// if (numA % 2 == 0) Console.WriteLine("Четное");
+// else Console.WriteLine("Нечетное");
 
 
- 
