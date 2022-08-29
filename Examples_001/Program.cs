2bdcbb5eed0126b5b0f742f99deced7fb018c318
Tima <failcrew2@icloud.com>
2022-08-29 13:05:19 +0300

    Дз 3 семинар

--- Examples_001/Program.cs
+++ Examples_001/Program.cs
@@ -1,69 +1,78 @@
-﻿// //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+﻿
+// //Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-// Console.WriteLine("Какой сегодня день недели?");
-// int weekday = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// string? number = Console.ReadLine();
 
-//     if (weekday <= 7)
-//     {
-//     if (weekday == 1)
-//     Console.Write("Сегодня понедельник - будний день. "); 
-//         else if (weekday == 2)
-//             Console.Write("Сегодня Вторник - будний день");
-//         else if    (weekday == 3)
-//             Console.Write("Сегодня Среда - будний день");
-//         else if    (weekday == 4)  
-//             Console.Write("Сегодня Четверг - будний день");
-//         else if    (weekday == 5)       
-//             Console.Write("Сегодня Пятница - будний день");
-//         else if    (weekday == 6) 
-//             Console.Write("Сегодня Суббота - Выходной день");
-//         else if    (weekday == 7) 
-//             Console.Write("Сегодня Воскресение - Выходной день");    
-//        }
-//     else
-//         Console.Write(" Дней в неделе всего 7 ;) ");
-
-    
-
-// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-
-//     Console.Write(" Введите трехзначное число ");
-// int NumberA = Convert.ToInt32(Console.ReadLine());
-// int NumberB = 0;
-
-//   if (100 <= NumberA && NumberA <= 999 )
-//     {
-//         NumberB = NumberA % 10;
-//         Console.WriteLine(NumberB + " Последняя цифра числа ");
-//     }
-//   else
+// void CheckingNumber(string number)
+// {
+//   if (number[0]==number[4] || number[1]==number[3])
 //         {
-//         Console.WriteLine("Вы ввели не трехзначное число"); 
+//             Console.WriteLine($"Ваше число: {number} - палиндром.");
 //         }
+//   else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+// }
+
+// if (number!.Length == 5)
+//         {
+//         CheckingNumber(number);
+//         }
+// else Console.WriteLine($"Введи правильное число");
+
+// //Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.WriteLine("Введите число: ");
+// int cube = Convert.ToInt32(Console.ReadLine());
+
+// void Cube(int[] cube)
+//     {
+//         int counter = 0;
+//         int length = cube.Length;
+//         while (counter <  length){
+//             cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+//             counter++;
+//         }
+//     }
+
+// void PrintArry(int[] coll)
+//     {
+//   int count = coll.Length;
+//   int index = 0;
+//   while(index < count)
+//         {
+//             Console.Write(coll[index]+ " ");
+//             index++;
+//         }
+//     } 
+
+// int[] arry = new int[cube+1];
+// Cube(arry);
+// PrintArry(arry);
 
 
+// // Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
+// int x1 = Coordinate("x", "A");
+// int y1 = Coordinate("y", "A");
+// int z1 = Coordinate("z", "A");
+// int x2 = Coordinate("x", "B");
+// int y2 = Coordinate("y", "B");
+// int z2 = Coordinate("z", "B");
 
-// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// int Coordinate(string coorName, string pointName)
+// {
+//     Console.Write($"Введите координату {coorName} точки {pointName}: ");
+//     return Convert.ToInt16(Console.ReadLine());
+// }
 
-Console.WriteLine(" Введите трехзначное число ");
-int NumberA = Convert.ToInt32(Console.ReadLine());
-int NumberB = 0;
+// double Decision(double x1, double x2, 
+//                 double y1, double y2, 
+//                 double z1, double z2){
+//   return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+//                    Math.Pow((y2-y1), 2) + 
+//                    Math.Pow((z2-z1), 2));
+// }
 
-  if (100 <= NumberA && NumberA <= 999)
-    {
-    NumberB = NumberA % 100 / 10;
-    Console.WriteLine(NumberB + " Вторая цифра числа ");
-    }
-else
-{
-  Console.WriteLine(" Вы ввели не трех значное число");
-}
+// double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
 
-//  // Это второй вариант решения задачи которая выше)) Но он меня не утроил т.к это работает с двухзначными. 
-
-// Console.Write("Введите трёхзначное число: ");
-// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-// string stringNumber = Convert.ToString(threeDigitNumber);
-// Console.WriteLine("вторая цифра этого числа "+stringNumber[1]);
+// Console.WriteLine($"Длина отрезка  {segmentLength}");
