610b507aadfae765b029f166b7dccd6d0533cec2
Tima <failcrew2@icloud.com>
2022-09-01 17:25:21 +0300

    Дз 4 семинар

--- Examples_001/Program.cs
+++ Examples_001/Program.cs
@@ -1,78 +1,48 @@
-﻿
-// //Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿// // Задача 25: Напишите цикл, который принимает на вход 
+// //два числа (A и B) и возводит число A в натуральную степень B.
 
-// Console.Write("Введите число: ");
-// string? number = Console.ReadLine();
+// Console.WriteLine("Введите число А");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// Summ(numA, numB);
 
-// void CheckingNumber(string number)
-// {
-//   if (number[0]==number[4] || number[1]==number[3])
+// void Summ(int numA, int numB)
 //         {
-//             Console.WriteLine($"Ваше число: {number} - палиндром.");
+//             Console.WriteLine(Math.Pow(numA, numB));
 //         }
-//   else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
-// }
 
-// if (number!.Length == 5)
-//         {
-//         CheckingNumber(number);
-//         }
-// else Console.WriteLine($"Введи правильное число");
 
-// //Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-// Console.WriteLine("Введите число: ");
-// int cube = Convert.ToInt32(Console.ReadLine());
+// // Задача 27: Напишите программу, которая принимает 
+// // на вход число и выдаёт сумму цифр в числе.
 
-// void Cube(int[] cube)
+// Console.WriteLine("Введите число");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// int numB = 0;
+// while (numA > 0)
 //     {
-//         int counter = 0;
-//         int length = cube.Length;
-//         while (counter <  length){
-//             cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-//             counter++;
-//         }
+//         numB = numB + numA % 10;
+//         numA = numA / 10; 
 //     }
+//  Console.WriteLine(numB + " Сумма цифр в вашем числе ");
 
-// void PrintArry(int[] coll)
-//     {
-//   int count = coll.Length;
-//   int index = 0;
-//   while(index < count)
-//         {
-//             Console.Write(coll[index]+ " ");
-//             index++;
-//         }
-//     } 
+// Задача 29: Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
 
-// int[] arry = new int[cube+1];
-// Cube(arry);
-// PrintArry(arry);
+int size = 8;
+Random myRandom = new Random();
+int[] array = new int [size];
+CheckArray(array);
 
+void CheckArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = myRandom.Next(8);
+        Console.Write("{0} ", array[i]);
+    }
+    
+}
 
-// // Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-
-// int x1 = Coordinate("x", "A");
-// int y1 = Coordinate("y", "A");
-// int z1 = Coordinate("z", "A");
-// int x2 = Coordinate("x", "B");
-// int y2 = Coordinate("y", "B");
-// int z2 = Coordinate("z", "B");
-
-// int Coordinate(string coorName, string pointName)
-// {
-//     Console.Write($"Введите координату {coorName} точки {pointName}: ");
-//     return Convert.ToInt16(Console.ReadLine());
-// }
-
-// double Decision(double x1, double x2, 
-//                 double y1, double y2, 
-//                 double z1, double z2){
-//   return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-//                    Math.Pow((y2-y1), 2) + 
-//                    Math.Pow((z2-z1), 2));
-// }
-
-// double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
-
-// Console.WriteLine($"Длина отрезка  {segmentLength}");
+ // Я наверное не правильно понял формулировку этой задачи?
